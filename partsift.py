03b6133bf50ef6471afadc9d55a0fbe0fe1f4843
trenzafeeds <guthriefacebook@gmail.com>
2018-02-23 18:05:54 -0500

    Added features
    Added sorting by coefficient and option for tuple output

--- partsift.py
+++ partsift.py
@@ -1,6 +1,6 @@
 """
 Kat Cannon-MacMartin
-partsift v1.0
+partsift v1.1
 A tool for building and polynomials and finding monomials.
 for use in the paper:
 'Sequences in Dihedral Groups with Distinct Partial Products'
@@ -28,21 +28,21 @@ def build_polynomial(r, s, degree = "false"):
 			polynomial = polynomial *term
 			poly_degree += 1
 
-		
+
 	for j in xrange(2, s+1):
 		for i in xrange(1, j):
 			term = (var("y" + str(j)) - var("y" + str(i)))
 			polynomial = polynomial*term
 			poly_degree += 1
 
-	
+
 
 	def z_adder(input):
 		z_added = 0
 		for i in xrange(1, input+1):
 			z_added = z_added + var("x"+str(i))
-		return z_added 
-	
+		return z_added
+
 
 	def findz(input, p):
 		if input == 0:
@@ -57,7 +57,7 @@ def build_polynomial(r, s, degree = "false"):
 						final = final + var("y" + str(l))
 					else:
 						final = final - (var("y" + str(l)))
-					
+
 				return z_adder(p) + final
 			else:
 				return z_adder(input)
@@ -91,7 +91,7 @@ def build_polynomial(r, s, degree = "false"):
 			return t_adder(q + 1, p) + final
 		else:
 			return t_adder(input, p)
-	
+
 
 	for j in xrange(2, p+q+2):
 		for i in xrange(1, j):
@@ -99,17 +99,17 @@ def build_polynomial(r, s, degree = "false"):
 				term = (findt(j, p, q)) - (findt(i, p, q))
 				polynomial = polynomial * term
 				poly_degree +=1
-	
+
 	if degree != "false" and degree != "False":
 		result_list = [polynomial, poly_degree]
 		return result_list
 	else:
 		return polynomial
-	
 
 
-	
-def find_monomials(polynomial, poly_degree, r, s):
+
+
+def find_monomials(polynomial, poly_degree, r, s, form = "list", sort_type = "zero"):
 	for num in xrange(1, r+1):
 		var("x"+str(num))
 
@@ -118,7 +118,7 @@ def find_monomials(polynomial, poly_degree, r, s):
 
 	polynomial_expanded = polynomial.expand()
 
-	test_monomial = 1	#This will be the base for all monomials we test with coefficient()
+	test_monomial = 1
 
 	for number_of_xs in xrange(1, r+1):
 		test_monomial = test_monomial*var("x"+str(number_of_xs))**var("xpower"+str(number_of_xs))
@@ -132,16 +132,16 @@ def find_monomials(polynomial, poly_degree, r, s):
 	list_of_ypowers = []
 	for number_of_ys in xrange(1, s+1):
 		list_of_ypowers.append(var("ypower"+str(number_of_ys)))
-	
+
 	list_of_both_powers = list_of_xpowers + list_of_ypowers
 	del(list_of_xpowers)
 	del(list_of_ypowers)
 
-	list_of_power_values = []	
+	list_of_power_values = []
 
 	for partition_set in Partitions(poly_degree, max_length=r+s).list():
 		partition_set = list(partition_set)
-		if max(partition_set)<max(r,s):	
+		if max(partition_set)<max(r,s):
 			if len(partition_set)==r+s:
 				list_of_power_values.append(partition_set)
 			else:
@@ -157,14 +157,24 @@ def find_monomials(polynomial, poly_degree, r, s):
 			if max(power_set[0:r])<r and max(power_set[r:r+s])<s:
 				temp_test_monomial = temp_test_monomial.subs({list_of_both_powers[i]:power_set[i] for i in xrange(0, r+s)})
 				if polynomial_expanded.coefficient(temp_test_monomial) not in [0]:
-					workable_monomials.append(polynomial_expanded.coefficient(temp_test_monomial)*temp_test_monomial)
-					
-					
+					workable_monomials.append((polynomial_expanded.coefficient(temp_test_monomial)*temp_test_monomial, int(polynomial_expanded.coefficient(temp_test_monomial))))
+
+	if len(workable_monomials) > 0:
+		if sort_type == "zero":
+			workable_monomials = sorted(workable_monomials, key = lambda tup: abs(tup[1]))
+		else:
+			workable_monomials = sorted(workable_monomials, key = lambda tup: tup[1])
+
+	if  form == "list":
+		for i in xrange(0, len(workable_monomials)):
+			workable_monomials[i] = workable_monomials[i][0]
+
 	return workable_monomials
 
 
-def sift(r, s):
+def sift(r, s, form = "list", sort_type = "zero"):
 	poly_input = build_polynomial(r, s, degree = "true")
-	return find_monomials(poly_input[0], poly_input[1], r, s)
+	return find_monomials(poly_input[0], poly_input[1], r, s, form = form, sort_type = sort_type)
+
 
 
